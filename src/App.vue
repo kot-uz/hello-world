19e64604b8bce43837433529d20a2dab1d005976
kot-uz <38581970+kot-uz@users.noreply.github.com>
2019-10-29 11:52:54 +0500

    Added some code

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,69 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header style="text-align: right; font-size:12px">
+        <div>
+          <el-menu  class="el-menu-demo" mode="horizontal">
+            <el-menu-item index="1">Processing Center</el-menu-item>
+            <el-submenu index="2">
+              <template slot="title">Workspace</template>
+              <el-menu-item index="2-1">item one</el-menu-item>
+              <el-menu-item index="2-2">item two</el-menu-item>
+              <el-menu-item index="2-3">item three</el-menu-item>
+              <el-submenu index="2-4">
+                <template slot="title">item four</template>
+                <el-menu-item index="2-4-1">item one</el-menu-item>
+                <el-menu-item index="2-4-2">item two</el-menu-item>
+                <el-menu-item :route="{name: 'about'}" index="2-4-3">About</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="3" disabled>Info</el-menu-item>
+            <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
+          </el-menu>
+        </div>
+      </el-header>
+      <el-main>
+        <span>{{ testVar }}</span>
+          <ul>
+            <li v-for="(testArr, index) in testArray" v-bind:key='index'>{{ ++index}} {{ testArr }}
+            <span style="{color: '#f50000';}" v-if="testArr == 'Вика'">{{ girlsName }}</span>
+            </li>
+          </ul>
+          <el-input placeholder="Please input" v-model="input"></el-input>
+          <el-button @click="pushArray()" >Default</el-button>
+       </el-main>
+    </el-container>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      testVar: 'First independent variable',
+      activeIndex: '1',
+      testArray: [
+        'first', 'second', 'third'
+      ],
+      products: [],
+      input: "",
+      girlsName: "!!!Вика detected!!!"
+    }
+  },
+  methods: {
+    handleSelect () {
+      console.log('Handle select methed')
+    },
+    pushArray() {
+      this.testArray.push(this.input);
+    }
+  },
+  created: {
+    //fetch('https://api.myjson.com/bins/74l63').then(response => response.json()).then(json => { this.products = json.products})
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,18 +72,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
