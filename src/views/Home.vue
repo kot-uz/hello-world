82cc7a00be7a06d659c475b2f4bf2e1103f066d2
kot-uz <38581970+kot-uz@users.noreply.github.com>
2019-10-29 17:23:54 +0500

    29102019 17:23

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 //First commit 29102019 17:20
+//Second commit without push
 export default {
   name: 'home',
   components: {
